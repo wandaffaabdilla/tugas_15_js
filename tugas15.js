7304685679482cf6e58ad4fda63392a718f3e9ef
wan daffa abdilla <wandaffaadilla@gmail.com>
2022-02-15 18:10:24 +0800

    Tugas 15 JS
    Tugas ini kami kumpulkan untuk niomic

---
+++ tugas15.js
@@ -0,0 +1,7 @@
+var ab = 8;
+var bc = 6;
+var ac = Math.sqrt(Math.pow(ab,2) + Math.pow(bc,2));
+
+console.log("Nilai AB =",(ab));
+console.log("Nilai BC =",(bc));
+console.log("Panjang sisi AC pada siku-siku tersebut adalah =",(ac));
